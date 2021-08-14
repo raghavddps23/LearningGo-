796b339c9c34be6013d0ddca91a6cfaf1679fdf8
Yusuf Taiwo Hassan <teehazzan@gmail.com>
2021-08-14 21:34:39 +0100

    implemented completely mergehlaves

--- Medium-Questions/MergeSort/MergeSort.go
+++ Medium-Questions/MergeSort/MergeSort.go
@@ -7,15 +7,58 @@ import (
 func main() {
 
 	fmt.Print("Here we go. Merge sort\n")
+	array := []int{2, 7, 11, 13, 3, 19, 5, 17, 88}
+	mergeHalves(array, 0, 4, 9)
 }
 
-func mergeSort(arr []int, start int, end int) {
+func MergeSort(arr []int, start int, end int) {
 	if start >= end {
 		return
 	}
-	middle := (start + end) / 2
+	var middle int = (start + end) / 2
 
-	mergeSort(arr, start, middle)
-	mergeSort(arr, middle+1, end)
+	MergeSort(arr, start, middle)
+	MergeSort(arr, middle+1, end)
 	//merge diff ones
+	mergeHalves(arr, start, middle, end)
+
+}
+
+func mergeHalves(arr []int, start int, middle int, end int) {
+	//create temporary arrays for both Halves
+	lSize := middle - start + 1
+	rSize := end - middle
+
+	//copy elements
+	left := arr[start:(start + lSize)]
+	right := arr[(middle + 1):(middle + rSize)]
+
+	//index for both arrays and output array
+	var i, j, k = 0, 0, 0
+	for i < lSize && j < rSize {
+		if left[i] <= right[j] {
+			arr[k] = left[i]
+			i++
+		} else {
+			arr[k] = right[j]
+			j++
+		}
+		k++
+
+	}
+	//copy remainder if exists
+	for i < lSize {
+		arr[k] = left[i]
+		i++
+		k++
+	}
+	//copy remainder if exists
+
+	for j < lSize {
+		arr[k] = right[j]
+		j++
+		k++
+	}
+	fmt.Print(arr)
+	fmt.Print("\n")
 }
